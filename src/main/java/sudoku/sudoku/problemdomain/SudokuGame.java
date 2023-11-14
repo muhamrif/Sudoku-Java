3e4170480f44dfec165c99918978d5afe608220b
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-11-14 17:36:12 -0500

    init setup

---
+++ src/main/java/sudoku/sudoku/problemdomain/SudokuGame.java
@@ -0,0 +1,18 @@
+package sudoku.sudoku.problemdomain;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+    public static final int GRID_BOUNDARY=9;
+
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public int[][] getCopyOfGridState(){
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+}
