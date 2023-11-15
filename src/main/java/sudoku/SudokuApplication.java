1845f17171d1a2204fd2d84dabe20dc3bb3bca7e
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-11-14 21:42:46 -0500

    UserInterfaceImpl done

--- src/main/java/sudoku/SudokuApplication.java
+++ src/main/java/sudoku/SudokuApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sudoku.userinterface.IUserInterfaceContract;
+import sudoku.userinterface.UserInterfaceImpl;
 
 import java.io.IOException;
 
